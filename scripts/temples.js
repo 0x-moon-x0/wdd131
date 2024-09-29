04df535f2ba57db8cbbee88e0bf301607917b37d
0x-moon-x0 <dru23001@byui.edu>
2024-09-29 11:41:08 -0600

    Temples album webpage WIP

---
+++ scripts/temples.js
@@ -0,0 +1,23 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navbar');
+const heading = document.getElementById('pg-heading');
+const navLinks = document.querySelectorAll('.navbar a')
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+function updateHeading(menuSelection) {
+    // update heading to match menu selection
+    heading.textContent = menuSelection;
+
+    // remove 'active' class from all links
+    navLinks.forEach(link => link.classList.remove('active'));
+
+    // find the selected link by matching its text with menuSelection and make it 'active'
+    const clickedLink = Array.from(navLinks).find(link => link.textContent === menuSelection);
+    if (clickedLink) {
+        clickedLink.classList.add('active');
+    }
+}
