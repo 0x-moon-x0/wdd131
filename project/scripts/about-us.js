05d5e6edf27469a27d54ae0fe3b867fbc5e4dd4a
0x-moon-x0 <dru23001@byui.edu>
2024-10-30 18:57:45 -0600

    about us mobile complete

--- project/scripts/about-us.js
+++ project/scripts/about-us.js
@@ -1,2 +1,14 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navbar');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+function updateHeading(menuSelection) {
+    document.getElementById('pg-heading').textContent = menuSelection;
+}
+
 document.getElementById("currentyear").innerHTML = new Date().getFullYear();
 document.getElementById("lastModified").innerHTML = document.lastModified;
